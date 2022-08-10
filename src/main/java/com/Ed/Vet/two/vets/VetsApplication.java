bcd0b7bc0dfd3b141bc7ad038ec3b311c273ed47
Ed <edwardbromilow@live.co.uk>
2022-08-10 16:53:09 +0100

    first commit

---
+++ src/main/java/com/Ed/Vet/two/vets/VetsApplication.java
@@ -0,0 +1,15 @@
+package com.Ed.Vet.two.vets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(VetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ed/Vet/two/vets/VetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ed.Vet.two.vets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
