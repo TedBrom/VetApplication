ce028432f1dfcf0c044e29e8af4c594de197b322
Ed <edwardbromilow@live.co.uk>
2022-08-11 15:32:19 +0100

    more tests added for sonar cloud

--- src/test/java/com/Ed/Vet/two/vets/PigeonTest.java
+++ src/test/java/com/Ed/Vet/two/vets/PigeonTest.java
@@ -30,4 +30,28 @@ public class PigeonTest
         FlyingRat testPige3 = new FlyingRat();
         assertEquals(false, testPige3.isDead());
     }
+    @Test
+    void testGetColour()
+    {
+        FlyingRat testPige4 = new FlyingRat();
+        assertEquals("Blue", testPige4.getColour());
+    }
+    @Test
+    void getFlying()
+    {
+        FlyingRat testPige5 = new FlyingRat();
+        assertEquals(true, testPige5.takeOff());
+    }
+    @Test
+    void getFlapping()
+    {
+        FlyingRat testPige6 = new FlyingRat();
+        assertEquals(true, testPige6.flap());
+    }
+    @Test
+    void getLand()
+    {
+        FlyingRat testPige7 = new FlyingRat();
+        assertEquals(true, testPige7.landing());
+    }
 }
